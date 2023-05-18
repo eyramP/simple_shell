b53078679dc275015ae6bc38b3b16111ba440597
Amosamevor <amosamevor@gmail.com>
2023-05-18 19:12:47 +0000

    get_error and _builtin

--- get_builtin.c
+++ get_builtin.c
@@ -1,10 +1,12 @@
 #include "main.h"
 
 /**
- * get_builtin - builtin that pais the command in the arg
+ * get_builtin - function builtin that pais the command in the arg
  * @cmd: command
+ *
  * Return: function pointer of the builtin command
  */
+
 int (*get_builtin(char *cmd))(data_shell *)
 {
 	builtin_t builtin[] = {
--- get_error.c
+++ get_error.c
@@ -1,11 +1,15 @@
 #include "main.h"
 
 /**
- * get_error - calls the error according the builtin, syntax or permission
+ * get_error - this function calls the error
+ * according the builtin, syntax or permission
+ *
  * @datash: data structure that contains arguments
  * @eval: error value
+ *
  * Return: error
  */
+
 int get_error(data_shell *datash, int eval)
 {
 	char *error;
